70d9a2d4673453082085a1defb7872c088226b6e
SimoSca <impetus_deep@ymail.com>
2015-04-11 22:15:58 +0200

    inserito untracked initFirst.sh

---
+++ initFirst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# comandi da runnare nella presente directory
+
+# installo le dipendenze
+npm install 
+
+# installo globalmente grunt-cli e fs
+sudo npm install -g grunt-cli
+sudo npm install -g fs
+
+# run grunt script
+grunt init-composer-propel
